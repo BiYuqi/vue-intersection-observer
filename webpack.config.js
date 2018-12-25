5e3b372f2c82455f0883e79db680de4502a1825d
biyuqi <biyuqiwan@163.com>
2018-12-25 21:12:58 +0800

    upgrade webpack-devpserver to 3.1.14. fixed Invalid Host/Origin Header warning

--- webpack.config.js
+++ webpack.config.js
@@ -61,8 +61,7 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     noInfo: true,
-    overlay: true,
-    disableHostCheck: true
+    overlay: true
   },
   performance: {
     hints: false
